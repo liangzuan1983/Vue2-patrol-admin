f50c2b9b6204f57da75ea40cbc77ed18e2b69f24
LKen <liangchuq@163.com>
2018-09-04 19:24:51 +0800

    update mock.js

--- src/mock/common-controller.js
+++ src/mock/common-controller.js
@@ -1,5 +1,28 @@
+import Mock from 'mockjs'
 export default {
   selectSubSystemConfig(config) {
-    return { 'ret': 1, 'data': { 'content': [{ 'id': 1, 'systemAccount': 'admin', 'systemCode': '001', 'systemIp': '120.79.154.168', 'systemName': 'robotservice', 'systemPassword': '21232f297a57a5a743894a0e4a801fc3', 'systemPort': '8080' }, { 'id': 2, 'systemAccount': '123456', 'systemCode': '002', 'systemIp': '192.168.41.14', 'systemName': 'gatemachine', 'systemPassword': '123456', 'systemPort': '80' }, { 'id': 3, 'systemAccount': '123', 'systemCode': '003', 'systemName': '1' }, { 'id': 4, 'systemAccount': '234', 'systemCode': '004', 'systemName': '2' }, { 'id': 5, 'systemAccount': '345', 'systemCode': '005', 'systemName': '3' }], 'first': true, 'last': false, 'number': 0, 'numberOfElements': 5, 'size': 5, 'totalElements': 8, 'totalPages': 2 }}
+    return Mock.mock({
+      'ret': 1,
+      'data': {
+        'content|3-10': [
+          {
+            'id|+1': 1,
+            'systemAccount': '@name',
+            'systemCode|10-1000': 1,
+            'systemIp': '@ip',
+            'systemName': 'robotservice',
+            'systemPassword': '21232f297a57a5a743894a0e4a801fc3',
+            'systemPort|5000-8000': 1
+          }
+        ],
+        'first': true,
+        'last': false,
+        'number': 0,
+        'numberOfElements': 5,
+        'size': 5,
+        'totalElements': 8,
+        'totalPages': 2
+      }
+    })
   }
 }
