4af13db381160357213bc50a7ce0f030146f123f
LKen <liangchuq@163.com>
2018-09-06 14:39:58 +0800

    删除api上传

--- src/api/gate-machine-controller.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function selectPassPersonInfo(params) {
-  return request({
-    url: '/api/selectPassPersonInfo',
-    method: 'get',
-    params
-  })
-}
